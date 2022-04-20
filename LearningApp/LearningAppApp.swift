32c7050b3128d6ca551e02efb88551b144f2fdc0
John <johnathanlee72@hotmail.com>
2022-04-20 09:29:33 -0700

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Johnathan Lee on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
