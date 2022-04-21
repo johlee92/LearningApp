ef39403581b09c9292611b4c6331d923b0296dc4
John <johnathanlee72@hotmail.com>
2022-04-20 23:51:02 -0700

    - Add dummy data - Add data parser

--- LearningApp/ViewModels/ContentModel.swift
+++ LearningApp/ViewModels/ContentModel.swift
@@ -9,4 +9,41 @@ import Foundation
 
 class ContentModel: ObservableObject {
     
+    @Published var modules = [Module]()
+    var styleData: Data?
+    
+    init() {
+        getLocalData()
+    }
+    
+    func getLocalData() {
+        let jsonUrl = Bundle.main.url(forResource: "data", withExtension: "json")
+        
+        do {
+            
+            let data = try Data(contentsOf: jsonUrl!)
+            let decoder = JSONDecoder()
+            
+            do {
+                
+                let moduleData = try decoder.decode([Module].self, from: data)
+                
+                self.modules = moduleData
+                
+            } catch {
+                print(error)
+            }
+        } catch {
+            print(error)
+        }
+        
+        let styleUrl = Bundle.main.url(forResource: "style", withExtension: "html")
+        
+        do {
+            let styleData = try Data(contentsOf: styleUrl!)
+            self.styleData = styleData
+        } catch {
+            print(error)
+        }
+    }
 }
